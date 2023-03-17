a5b1dabeddee466f359fa7d686b4ca04d4a0e5e7
Sarah Susan <55784685+velvetrose@users.noreply.github.com>
2023-03-17 18:22:36 +0530

    added loading function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,34 @@ export class AppComponent {
   advices:any;
   adviceID!:string;
   adviceQuote!:string;
+  isLoading!:boolean;
+  randomizeButton!:boolean;
 
   constructor(private advice:AdviceService){}
 
   ngOnInit(){
+    this.isLoading=true;
+    this.randomizeButton=false;
+
     this.advice.getAdvices().subscribe(data =>{
       this.advices=data;
       console.log(this.advices);
       console.log(this.advices.slip.id);
       console.log(this.advices.slip.advice);
+      this.isLoading=false;
+      this.randomizeButton=true;
       this.adviceID=`ADVICE #${this.advices.slip.id}`;
       this.adviceQuote=`"${this.advices.slip.advice}"`;
     })
   }
 
   randomizeQuote(){
+    this.isLoading=true;
+    this.randomizeButton=false;
     this.advice.getAdvices().subscribe(data =>{
       this.advices=data;
+      this.isLoading=false;
+      this.randomizeButton=true;
       this.adviceID=`ADVICE #${this.advices.slip.id}`;
       this.adviceQuote=`"${this.advices.slip.advice}"`;
     })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
